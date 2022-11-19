64206e7fbae64ca4b7aa6d00f8ab1ebdf4a15505
Pavel <verner_pv@mail.ru>
2022-11-20 00:04:45 +0700

    Add base bot logic

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def start(message):
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
-    json_string = request.get_data().decode("utf=8")
+    json_string = request.get_data().decode("utf-8")
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return"!", 200
