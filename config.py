c75f9f275d73653bccfc4a02c83ba6609132526f
Pavel <verner_pv@mail.ru>
2022-11-19 23:28:35 +0700

    Add base bot logic

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN="5950820329:AAFbXajB23osRMLTg1xi_CJudTlL4o7N_xg"
-APP_URL="https://habitresetbase.herokuapp.com/" + BOT_TOKEN
+BOT_TOKEN = "5950820329:AAFbXajB23osRMLTg1xi_CJudTlL4o7N_xg"
+APP_URL = "https://habitresetbase.herokuapp.com/" + BOT_TOKEN
