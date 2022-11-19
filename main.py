45763ab6f203a4384f55bab789e787706d284f3e
Pavel <verner_pv@mail.ru>
2022-11-19 22:50:23 +0700

    Add base bot logic

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logger=telebot.logger
 logger.setLevel(logging.DEBUG)
 
 
-@bot.message_handler(command=["start"])
+@bot.message_handler(command=["/start"])
 def start(message):
     username=message.from_user.username
     bot.reply_to(message,f"Hello,{username}!")
@@ -23,7 +23,7 @@ def redirect_message():
     return"!",200
 
 
-if __name__="__name__":
+if __name__=="__name__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
     server.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
