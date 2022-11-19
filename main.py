6b66e8e36597a0a12965df260be387b1d80a07fd
Pavel <verner_pv@mail.ru>
2022-11-19 23:21:04 +0700

    Add base bot logic

--- main.py
+++ main.py
@@ -2,28 +2,28 @@ import os
 import telebot
 import logging
 from config import *
-from flask import Flask,request
-bot=telebot.Telebot(BOT_TOKEN)
-server=Flask(__name__)
-logger=telebot.logger
+from flask import Flask, request
+bot = telebot.TeleBot(BOT_TOKEN)
+server = Flask(__name__)
+logger = telebot.logger
 logger.setLevel(logging.DEBUG)
-print("sa")\
+print("sa")
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=["strt"])
 def start(message):
-    username=message.from_user.username
-    bot.reply_to(message,f"Hello,{username}!")
+    username = message.from_user.username
+    bot.reply_to(message, f"Hello,{username}!")
 
 
-@server.route(f"/{BOT_TOKEN}",methods=["POST"])
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
-    json_string=request.get_data().decode("utf=8")
-    update=telebot.types.Update.de_json(json_string)
-    bor.process_new_updates([update])
-    return"!",200
+    json_string = request.get_data().decode("utf=8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return"!", 200
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
-    server.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
