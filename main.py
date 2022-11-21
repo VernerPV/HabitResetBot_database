29102dacd0b80c3161422020a079a7b6e67b6042
Pavel <verner_pv@mail.ru>
2022-11-21 23:17:13 +0700

    Add base bot logic

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ from telebot import types
 DATABASE_URL = os.getenv('DATABASE_URL')
 print('!!!!!!!!!!!!!!!!!!!!!!')
 BOT_TOKEN =S3Connection(os.environ['BOT_TOKEN'])
+print(BOT_TOKEN)
 APP_URL = os.getenv('APP_URL')
 DB_URI = os.getenv('DB_URI')
-print(BOT_TOKEN)
+
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
