5bcaf844ffb9852310d864fc758bace5d8141944
Pavel <verner_pv@mail.ru>
2022-11-26 21:54:52 +0700

    Add base bot logic

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import psycopg2
 from flask import Flask, request
 from telebot import types
 import schedule
-
+from telegram.ext import ContextTypes, Application
 import time
 
 
@@ -157,6 +157,18 @@ def redirect_message():
     bot.process_new_updates([update])
     return "!", 200
 
+
+
+
+async def callback_minute(context: ContextTypes.DEFAULT_TYPE):
+ await context.bot.send_message(chat_id='838386449', text='One message every minute')
+
+application = Application.builder().token(BOT_TOKEN).build()
+job_queue = application.job_queue
+
+job_minute = job_queue.run_repeating(callback_minute, interval=60, first=10)
+
+application.run_polling()
 
 #def schedule_checker():
 #    while True:
