f74ef806c29106f8bcd98a0b07347bed0e2aaa75
Pavel <verner_pv@mail.ru>
2022-11-27 17:33:18 +0700

    Add base bot logic

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
+import os
 from telegram.ext import ContextTypes, Application
-
+BOT_TOKEN = os.environ['BOT_TOKEN']
 async def callback_minute(context: ContextTypes.DEFAULT_TYPE):
- await context.bot.send_message(chat_id='838386449', text='One message every minute')
+ await context.bot.send_message(chat_id=838386449, text='One message every minute')
 
-application = Application.builder().token('TOKEN').build()
+application = Application.builder().token(BOT_TOKEN).build()
 job_queue = application.job_queue
 
 job_minute = job_queue.run_repeating(callback_minute, interval=60, first=10)
